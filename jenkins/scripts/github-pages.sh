45bbd0daf01a4c9b2a952659d27e000b187a4616
Jody <macbook@MacBooks-MacBook-Pro.local>
2025-02-24 17:59:04 +0700

    change branch name in git pages

--- jenkins/scripts/github-pages.sh
+++ jenkins/scripts/github-pages.sh
@@ -10,8 +10,11 @@ fi
 git config --global user.email "41898282+github-actions[bot]@users.noreply.github.com"
 git config --global user.name "github-actions[bot]"
 
+# Pastikan berada di branch react-app sebelum menjalankan gh-pages
+git checkout react-app || { echo "Branch react-app not found"; exit 1; }
+
 # Set the correct GitHub URL
 git remote set-url origin https://git:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git
 
-# Run gh-pages
-npx gh-pages --message '[skip ci] Updates' --dist build
+# Push ke GitHub Pages dari react-app
+npx gh-pages --message '[skip ci] Updates' --dist build --branch gh-pages
